f7cb5b25e066062da01d82da5cb02392a031679b
David Feinzimer <dfeinzimer@gmail.com>
2022-10-20 23:59:11 -0700

    Start new widgets extension

---
+++ Widgets/Widgets.swift
@@ -0,0 +1,169 @@
+//
+//  Widgets.swift
+//  Widgets
+//
+//  Created by David Feinzimer on 10/20/22.
+//  Copyright © 2022 LoopKit Authors. All rights reserved.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> CoffeeTrackerEntry {
+        CoffeeTrackerEntry(date: Date(),
+                           mgCaffeine: 250.0,
+                           totalCups: 2.0)
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (CoffeeTrackerEntry) -> Void) {
+        
+        if context.isPreview {
+            // Show a complication with generic data.
+            let entry = CoffeeTrackerEntry(date: Date(),
+                        mgCaffeine: 250.0,
+                        totalCups: 2.0)
+            
+            completion(entry)
+            return
+        }
+        
+        Task {
+            
+            let date = Date()
+            
+            // Get the current data from the model.
+            let mgCaffeine = 50.0
+            let totalCups = 0.5
+            
+            // Create the entry.
+            let entry = CoffeeTrackerEntry(date: date,
+                                    mgCaffeine: mgCaffeine,
+                                    totalCups: totalCups)
+            
+            // Pass the entry to the completion handler.
+            completion(entry)
+        }
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
+        Task {
+
+            // Create an array to hold the events.
+            var entries: [CoffeeTrackerEntry] = []
+            
+            // The total number of cups consumed only changes when the user actively adds a drink,
+            // so it remains constant in this timeline.
+            let totalCups = 0.5
+
+            // Generate a timeline covering every 5 minutes for the next 24 hours.
+            let currentDate = Date()
+            for minuteOffset in stride(from: 0, to: 60 * 24, by: 5) {
+                let entryDate = Calendar.current.date(byAdding: .minute, value: minuteOffset, to: currentDate)!
+                
+                // Get the projected data for the specified date.
+                let mgCaffeine = 100.0
+                
+                // Create the entry.
+                let entry = CoffeeTrackerEntry(date: entryDate,
+                                               mgCaffeine: mgCaffeine,
+                                               totalCups: totalCups)
+                
+                // Add the event to the array.
+                entries.append(entry)
+            }
+
+            // Create the timeline and pass it to the completion handler.
+            // Because the caffeine dose drops to 0.0 mg after 24 hours,
+            // there's no need to reload this timeline unless the user adds
+            // a new drink. Setting the reload policy to .never.
+            let timeline = Timeline(entries: entries, policy: .never)
+            
+            // Pass the timeline to the completion handler.
+            completion(timeline)
+        }
+    }
+}
+
+struct CoffeeTrackerEntry: TimelineEntry {
+    let date: Date
+    let mgCaffeine: Double
+    let totalCups: Double
+}
+
+struct CircularComplication: View {
+    /// The widget's rendering mode.
+    @Environment(\.widgetRenderingMode) var renderingMode
+    
+    var mgCaffeine: Double
+    var totalCups: Double
+    let maxMG = 500.0
+    
+    var body: some View {
+        Gauge( value: min(mgCaffeine, maxMG), in: 0.0...maxMG ) {
+            Text("mg")
+        } currentValueLabel: {
+            if renderingMode == .fullColor {
+                // Add a foreground color to the label.
+                Text(mgCaffeine.description)
+                    .foregroundColor(.red)
+            }
+            else {
+                // Otherwise, use the default text color.
+                Text(mgCaffeine.description)
+            }
+        }
+        .gaugeStyle(
+            // Add a gradient to the gauge.
+            CircularGaugeStyle(tint: .black)
+        )
+    }
+}
+
+struct WidgetsEntryView : View {
+    var entry: Provider.Entry
+    
+    /// The widget's family
+    @Environment(\.widgetFamily) private var family
+    
+    var body: some View {
+        Text(entry.date, style: .time)
+        
+        switch family {
+        case .accessoryCircular:
+            CircularComplication(
+                mgCaffeine: entry .mgCaffeine,
+                totalCups: entry.totalCups
+            )
+        case .accessoryCorner:
+            fatalError("Not implemented")
+        case .accessoryInline:
+            fatalError("Not implemented")
+        case .accessoryRectangular:
+            fatalError("Not implemented")
+        @unknown default:
+            fatalError("Not implemented")
+        }
+    }
+}
+
+@main
+struct Widgets: Widget {
+    let kind: String = "Widgets"
+    
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            WidgetsEntryView(entry: entry)
+        }
+        .configurationDisplayName("Loop Data By David")
+        .description("This is an example widget.")
+        .supportedFamilies([.accessoryCircular]) // TODO: Add support for remaining families
+    }
+}
+
+struct Widgets_Previews: PreviewProvider {
+    static var previews: some View {
+        WidgetsEntryView(entry: CoffeeTrackerEntry(date: .now, mgCaffeine: 40.0, totalCups: 0.75))
+            .previewContext(WidgetPreviewContext(family: .accessoryRectangular))
+    }
+}
